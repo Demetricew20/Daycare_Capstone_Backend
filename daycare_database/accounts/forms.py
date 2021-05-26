5beb898bd491fa4f4e3e93293e82e6f669bf447d
DemetriceW20 <demetrice_williams@hotmail.com>
2021-05-26 11:29:44 -0800

    Created projects and apps. Created CustomForm for User Model.

---
+++ daycare_database/accounts/forms.py
@@ -0,0 +1,28 @@
+from django import forms
+from .models import User
+from django.contrib.auth.models import Group
+from django.contrib.auth.forms import UserCreationForm
+
+
+class CustomUserForm(UserCreationForm):
+    """This form allows our custom user model to be registered with an added field reflecting its employee status"""
+    is_daycare = forms.BooleanField(label="Check to register daycare", required=False)
+
+    class Meta:
+        model = User
+        fields = ("username", "password1", "password2", "is_daycare")
+
+    def save(self, commit=True):
+        # Overriding the save method to add user to auth group of Employee or Customer depending on if box is checked
+        user = super(CustomUserForm, self).save(commit=False)
+        user.is_daycare = self.cleaned_data["is_daycare"]
+
+        if commit:
+            user.save()
+            if user.is_daycare:
+                daycare = Group.objects.get(name="Daycares")
+                daycare.user_set.add(user)
+            else:
+                parents = Group.objects.get(name="Parents")
+                parents.user_set.add(user)
+        return user
